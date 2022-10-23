4ccfc7371f9500e6f55107dfa36302cf26b95aff
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-10-23 13:34:14 +0100

    added both the github and slack icons.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header'
 import LinkContainer from './components/LinkContainer';
+import slackSvg from './components/assests/slack.svg'
+import githubSvg from './components/assests/Social icon.svg'
 
 function App() {
   return (
     <>
-          <div className="bg-white container mx-auto ">
+      <div className="bg-white container mx-auto ">
+        
       {/* header */}
         <Header />
         
       {/* links */}
       <LinkContainer/>
-    </div>
+      </div>
+      
+      <div className='flex justify-center items-center gap-4 pb-9 pt-4'>
+        <a href='https://slack.com/boivado2' target="_blank" rel="noopener noreferrer"><img className='' src={slackSvg} alt="slack-icon" /></a>
+        
+        <a href='https://github.com/boivado2' target="_blank" rel="noopener noreferrer"><img className='' src={githubSvg}  alt="github-icon"/></a>
+      </div>
+      
+
+    {/* footer */}
     <Footer/>
     </>
   );
