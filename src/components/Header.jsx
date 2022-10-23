ed1b7dc03dac184fadacb5754d86b8a28a2f868e
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-10-23 13:40:21 +0100

    remove the header section margin-bottom proprty.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import profile_url from './assests/me3.jpg'
 
 function Header() {
   return (
-    <div className='flex flex-col justify-center items-center mb-16 px-5 py-4 gap-3'>
+    <div className='flex flex-col justify-center items-center  px-5 py-4 gap-3'>
       <img src={profile_url} id="profile_url" className=' w-28 h-28 rounded-full' alt='profile_url
       ' />
       <h3 className='' id='twitter'>Ayebamondiafere Godwin</h3>
