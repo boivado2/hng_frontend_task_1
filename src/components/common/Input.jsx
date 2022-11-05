ef58ce1fc78ec9fc7c5a30507b1e7efb90efb1f9
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-11-05 21:01:25 +0100

    fix: fix the bug that prevent textarea input field from being focus when you click label.

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -4,7 +4,7 @@ function Input({ label, name, styles, ...other }) {
   console.log(label, name)
   return ( 
     <div className='flex flex-col justify-start items-start gap-4'>
-      <label htmlFor={name} className=" text-sm">{ label}</label>
+      <label htmlFor={name} className=" text-sm cursor-pointer">{ label}</label>
       <input  {...other} id={name} name={name} className={`${styles} text-base px-4 py-3 w-full border border-custom-gray-100 rounded-md outline-none`} />
     </div> 
   )
--- src/components/common/Textarea.jsx
+++ src/components/common/Textarea.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Textarea({id, label, name, styles, ...others}) {
+function Textarea({ label, name, styles, ...others}) {
   return (
-    <div className='flex flex-col items-start justify-start gap-3'>
-      <label htmlFor={id}>{label}</label>
-      <textarea id={id} name={name} className={`${styles} w-full h-[132px] border border-custom-gray-100 rounded-md outline-none resize-none px-4 py-3`} {...others}></textarea>
+    <div className='flex flex-col items-start justify-start gap-3 '>
+      <label htmlFor={name} className="cursor-pointer text-sm">{label}</label>
+      <textarea id={name} name={name} className={`${styles} w-full h-[132px] border border-custom-gray-100 rounded-md outline-none resize-none px-4 py-3`} {...others}></textarea>
     </div>
   )
 }
