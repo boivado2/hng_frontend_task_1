1a3006c5fa2f1019498b645a066c984ce6491448
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-11-05 15:27:14 +0100

    refactor: refactoring app component by moving all html element to home component.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import Footer from './components/Footer';
-import Header from './components/Header'
-import LinkContainer from './components/LinkContainer';
-import slackSvg from './components/assests/slack.svg'
-import githubSvg from './components/assests/Social icon.svg'
+import Home from './components/Home';
+
 import './App.css';
-import Contacts from './components/Contacts';
 
 
 
 function App() {
   return (
     <>
-      <div className="bg-white container mx-auto font-custom-open-sans">
-        
-      {/* header */}
-        <Header />
-        
-      {/* links */}
-      <LinkContainer/>
-      </div>
-      
-      <div className='flex justify-center items-center gap-4 pb-9 pt-4'>
-          <img className='' src={slackSvg} alt="slack-icon" />
-        
-          <img className='' src={githubSvg} alt="github-icon" />
-      </div>
+  
 
+      <Home />
+      
       
 
     {/* footer */}
---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Header'
+import LinkContainer from './LinkContainer'
+import slackSvg from './assests/slack.svg'
+import githubSvg from './assests/Social icon.svg'
+
+function Home() {
+  return (
+    <>
+          <div className="bg-white container mx-auto font-custom-open-sans">
+        
+        {/* header */}
+          <Header />
+          
+        {/* links */}
+        <LinkContainer/>
+        </div>
+        
+        <div className='flex justify-center items-center gap-4 pb-9 pt-4'>
+            <img className='' src={slackSvg} alt="slack-icon" />
+          
+            <img className='' src={githubSvg} alt="github-icon" />
+        </div>
+    </>
+  )
+}
+
+export default Home
